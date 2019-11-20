b7eda85c6c158b429d71db8685cc8c8fd58b3260
jb <jasontbrown99@gmail.com>
2019-11-20 12:11:18 -0800

    Add redux thunk and refactor shop component with container pattern

--- src/redux/shop/shop.actions.js
+++ src/redux/shop/shop.actions.js
@@ -1,7 +1,29 @@
 import ShopActionTypes from './shop.types';
+import { firestore, convertCollectionsShapshotToMap } from '../../firebase/firebase.utils';
 
-export const updateCollections = collectionsMap => ({
-  type: ShopActionTypes.UPDATE_COLLECTIONS,
-  payload: collectionsMap
+export const fetchCollectionsStart = () => ({
+  type: ShopActionTypes.FETCH_COLLECTION_START
 })
+
+export const fetchCollectionsSuccess = collectionsMap => ({
+  type: ShopActionTypes.FETCH_COLLECTION_SUCCESS,
+  payload: collectionsMap
+});
+
+export const fetchCollectionsFailure = errorMessage => ({
+  type: ShopActionTypes.FETCH_COLLECTION_FAILURE,
+  payload: errorMessage
+})
+
+export const fetchCollectionsStartAsync = () => {
+  return dispatch => {
+    const collectionRef = firestore.collection('collections');
+    dispatch(fetchCollectionsStart());
+
+    collectionRef.get().then(snapshot => {
+      const collectionsMap = convertCollectionsShapshotToMap(snapshot);
+      dispatch(fetchCollectionsSuccess(collectionsMap));
+    }).catch(error => dispatch(fetchCollectionsFailure(error.message)));
+  }
+}
 
